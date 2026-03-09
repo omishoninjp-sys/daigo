833178f5b9530e6306bb6733b3a1a4454eb5137b
omishoninjp-sys <omishoninjp@gmail.com>
2026-03-10 08:08:22 +0900

    Add files via upload

--- scrapers/grail.py
+++ scrapers/grail.py
@@ -81,7 +81,7 @@ class GrailMixin:
                 for img in soup.find_all("img"):
                     src = img.get("src", "")
                     if "cdn.grail.bz/images/goods" in src:
-                        src = src.replace("/images/goods/t/", "/images/goods/")
+                        src = src.replace("/images/goods/t/", "/images/goods/d/")
                         if src not in main_imgs:
                             main_imgs.append(src)
 
@@ -100,7 +100,7 @@ class GrailMixin:
                     # 例: /images/goods/t/ac1909/ac1909_col_11.jpg
                     #  → /images/goods/ac1909/ac1909_col_11.jpg
                     if color_img:
-                        color_img = color_img.replace("/images/goods/t/", "/images/goods/")
+                        color_img = color_img.replace("/images/goods/t/", "/images/goods/d/")
                     # 顏色名稱
                     color_name_el = li.find("p", class_="txt-info")
                     color_name = color_name_el.get_text(strip=True) if color_name_el else ""
