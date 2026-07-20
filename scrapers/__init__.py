2eb25fda136e0f59fea6a32193387cec59e79580
omishoninjp-sys <omishoninjp@gmail.com>
2026-07-20 11:00:35 +0800

    Add files via upload

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -69,12 +69,14 @@ from scrapers.makeshop import MakeShopMixin
 from scrapers.platform import register, get_platform, LegacyPlatform
 from scrapers.platform_zozotown import ZozotownPlatform
 from scrapers.platform_amiami import AmiamiPlatform
-from scrapers.platform_bookoff import BookoffPlatform 
+from scrapers.platform_bookoff import BookoffPlatform
+from scrapers.platform_snidel import SnidelPlatform 
 
 # 真 Platform 先註冊；LegacyPlatform 最後（catch-all）
 register(ZozotownPlatform())
 register(AmiamiPlatform())
 register(BookoffPlatform())
+register(SnidelPlatform())
 register(LegacyPlatform())
 
 
